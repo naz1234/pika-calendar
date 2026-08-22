17f9c0c548e6013595ea647aae2c24b692110d7e
Nazif Jaafar <nazif92@gmail.com>
2026-08-23 00:48:52 +0300

    Refresh installed calendar app cache
    Co-authored-by: nuursyafiqah <282911999+nuursyafiqah@users.noreply.github.com>

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "my-calendar-v";
-const CACHE_NAME = "my-calendar-v29";
+const CACHE_NAME = "my-calendar-v30";
 const APP_SHELL = [
   "/manifest.webmanifest",
   "/icons/calendar-192.png",
