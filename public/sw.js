f8616baf61ddca64983b912addf62a9664e03d71
Nazif Jaafar <nazif92@gmail.com>
2026-08-24 06:20:46 +0300

    feat: open shared roster files in a popup
    Co-authored-by: Nuur Syafiqah <nuursyafiqah9307@gmail.com>

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "my-calendar-v";
-const CACHE_NAME = "my-calendar-v42";
+const CACHE_NAME = "my-calendar-v43";
 const APP_SHELL = [
   "/manifest.webmanifest",
   "/icons/calendar-192.png",
