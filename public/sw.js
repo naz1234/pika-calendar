0e1c8c69692ffc4f2a1201fe97c15c7c006e32f6
Nazif Jaafar <nazif92@gmail.com>
2026-08-24 05:25:47 +0300

    fix: match calculator window colors
    Co-authored-by: Nuur Syafiqah <nuursyafiqah9307@gmail.com>

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "my-calendar-v";
-const CACHE_NAME = "my-calendar-v37";
+const CACHE_NAME = "my-calendar-v38";
 const APP_SHELL = [
   "/manifest.webmanifest",
   "/icons/calendar-192.png",
