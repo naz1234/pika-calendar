3652849a8835ae241d6d44b9aa1bf4f29dde8222
Nazif Jaafar <nazif92@gmail.com>
2026-08-27 00:51:48 +0300

    fix: isolate mobile date input sizing

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "my-calendar-v";
-const CACHE_NAME = "my-calendar-v46";
+const CACHE_NAME = "my-calendar-v47";
 const APP_SHELL = [
   "/manifest.webmanifest",
   "/icons/calendar-192.png",
